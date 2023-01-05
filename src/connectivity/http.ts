6fb46c5ebf80a68b6fd26872476c0b0c746dba92
Antoni Spaanderman <56turtle56@gmail.com>
2023-01-05 20:46:36 +0100

    refractor http server: allow adding new http request listeners from plugins

--- src/connectivity/http.ts
+++ src/connectivity/http.ts
@@ -28,14 +28,50 @@ function checkParams<R extends string, O extends string>(
 	util.checkParams(params, requiredParams, optionalParams, p => `query string parameter${p ? 's' : ''}`, extra);
 }
 
+type RequestListener<R extends string = string, O extends string = string> = (
+	params: { [k in R]: string } & { [k in O]?: string }
+) => string | void | Promise<string | void>;
+
 export default class implements Server {
+	private started = false;
 	private socket: string | number;
+	private requestListeners: { [name: string]: RequestListener } = {};
 
 	constructor(cfg?: typeof config['http']) {
 		this.socket = cfg?.socket || 3000;
+
+		this.registerRequestListener('setLed', [ 'name', 'index', 'color' ], [], async params => {
+			const color = util.objMap(JSON.parse(params.color), Number);
+			await ledstrip.setLed(params.name, parseInt(params.index), color);
+		});
+
+		this.registerRequestListener('fillLeds', [ 'name', 'color' ], [ 'start', 'end' ], async params => {
+			const color = util.objMap(JSON.parse(params.color), Number);
+			await ledstrip.fillLeds(params.name, color, util.parseIntIf(params.start), util.parseIntIf(params.end));
+		});
+
+		this.registerRequestListener('setLeds', [ 'name', 'data' ], [ 'start' ], async params => {
+			const data = JSON.parse(params.data);
+			if (!Array.isArray(data)) {
+				throw new Error(`Query string parameter "data" must be an array\n`);
+			}
+			await ledstrip.setLeds(
+				params.name,
+				data.map(x => util.objMap(x, Number)),
+				util.parseIntIf(params.start)
+			);
+		});
+
+		this.registerRequestListener('displayNumber', [ 'n' ], [ 'maxdecimals' ], async params => {
+			await display.updateDisplayFloat(Number(params.n), params.maxdecimals ? parseInt(params.maxdecimals) : 4);
+		});
 	}
 
 	start() {
+		if (this.started) {
+			return;
+		}
+		this.started = true;
 		const server = createServer((req, res) => this.requestListener(req, res));
 		if (typeof this.socket === 'string' && /:\d+/.test(this.socket)) {
 			const parts = this.socket.split(':');
@@ -46,11 +82,26 @@ export default class implements Server {
 		}
 	}
 
-	listeningListener() {
+	registerRequestListener<R extends string, O extends string>(
+		name: string,
+		requiredParams: readonly R[],
+		optionalParams: readonly O[],
+		listener: RequestListener<R, O>
+	) {
+		if (name in this.requestListeners) {
+			throw new Error(`Name "${name}" already in use`);
+		}
+		this.requestListeners[name] = params => {
+			checkParams(params, requiredParams, optionalParams);
+			return listener(params);
+		};
+	}
+
+	private listeningListener() {
 		console.log(`HTTP server listening on ${this.socket}`);
 	}
 
-	async requestListener(req: IncomingMessage, res: ServerResponse) {
+	private async requestListener(req: IncomingMessage, res: ServerResponse) {
 		try {
 			const url = new URL(`http://localhost/${req.url}`);
 			const args = url.pathname.split('/').filter(x => x.trim());
@@ -59,49 +110,14 @@ export default class implements Server {
 				res.writeHead(200);
 				res.end(readFileSync('index.html'));
 			} else if (args.length === 1) {
-				switch (args[0]) {
-					case 'setLed': {
-						checkParams(params, [ 'name', 'index', 'color' ], []);
-						const color = util.objMap(JSON.parse(params.color), Number);
-						await ledstrip.setLed(params.name, parseInt(params.index), color);
-						res.writeHead(200);
-						res.end(`Success\n`);
-						break;
-					}
-					case 'fillLeds': {
-						checkParams(params, [ 'name', 'color' ], [ 'start', 'end' ]);
-						const color = util.objMap(JSON.parse(params.color), Number);
-						await ledstrip.fillLeds(params.name, color, util.parseIntIf(params.start), util.parseIntIf(params.end));
-						res.writeHead(200);
-						res.end(`Success\n`);
-						break;
-					}
-					case 'setLeds': {
-						checkParams(params, [ 'name', 'data' ], [ 'start' ]);
-						const data = JSON.parse(params.data);
-						if (!Array.isArray(data)) {
-							throw new Error(`Query string parameter "data" must be an array\n`);
-						}
-						await ledstrip.setLeds(
-							params.name,
-							data.map(x => util.objMap(x, Number)),
-							util.parseIntIf(params.start)
-						);
-						res.writeHead(200);
-						res.end(`Success\n`);
-						break;
-					}
-					case 'displayNumber': {
-						checkParams(params, [ 'n' ], [ 'maxdecimals' ]);
-						await display.updateDisplayFloat(Number(params.n), params.maxdecimals ? parseInt(params.maxdecimals) : 4);
-						res.writeHead(200);
-						res.end(`Success\n`);
-						break;
-					}
-					default:
-						res.writeHead(404);
-						res.end('Not Found\n');
-						return;
+				if (args[0] in this.requestListeners) {
+					const ret = (await this.requestListeners[args[0]](params)) || 'Success';
+					res.writeHead(200);
+					res.end(`${ret}\n`);
+				} else {
+					res.writeHead(404);
+					res.end('Not Found\n');
+					return;
 				}
 			} else {
 				res.writeHead(404);
--- src/connectivity/index.ts
+++ src/connectivity/index.ts
@@ -5,9 +5,9 @@ export interface Server {
 	start(): void;
 }
 
-const availableServers: { [name in typeof config.servers[number]]: new (cfg: typeof config[name]) => Server } = {
+export const availableServers: Readonly<{ [name in typeof config.servers[number]]: new (cfg: typeof config[name]) => Server }> = Object.freeze({
 	http: HttpServer
-};
+});
 
 export function initServers() {
 	for (const s of config.servers) {
