8df96fc96a3baa768affcf8e95ba6f133d2b86d8
Antoni Spaanderman <56turtle56@gmail.com>
2023-08-03 00:38:53 +0200

    refactor usb led plugin a bit + change color

--- plugins/usb.sample.plugin.ts
+++ plugins/usb.sample.plugin.ts
@@ -1,7 +1,6 @@
 import { spawn } from 'child_process';
 import { basename } from 'path';
 import { ledstrip } from '../src/devices';
-import * as colorutil from '../src/colorutil';
 
 function watchUSBDevices(
 	cb: (update: { kernelTimestamp: number; action: string; data: { [k: string]: string } }) => void
@@ -35,6 +34,11 @@ function watchUSBDevices(
 	});
 }
 
+const colors = {
+	idle: { r: 0, g: 0, b: 30 },
+	connected: { r: 0, g: 50, b: 0 }
+};
+
 const ports: { [port: string]: number | { start: number; length: number } } = {
 	'1': 50,
 	'3': 51,
@@ -49,19 +53,18 @@ function setPortLed(port: string, color: { [k: string]: number }, update = true)
 	if (!led) {
 		return;
 	}
-	const start = typeof led === 'number' ? led : led.start;
-	const length = typeof led === 'number' ? 1 : led.length;
+	const { start, length } = typeof led === 'number' ? { start: led, length: 1 } : led;
 	ledstrip.setLeds('monitor_backlight', new Array(length).fill(color), start, update);
 }
 
 const portNames = Object.keys(ports);
 for (let i = 0; i < portNames.length; i++) {
-	setPortLed(portNames[i], { r: 50, g: 0, b: 0 }, i + 1 === portNames.length);
+	setPortLed(portNames[i], colors.idle, i + 1 === portNames.length);
 }
 
 watchUSBDevices(update => {
 	if (update.action === 'add' || update.action === 'remove') {
 		const port = basename(update.data.devpath).slice(2);
-		setPortLed(port, update.action === 'add' ? { r: 0, g: 50, b: 0 } : { r: 50, g: 0, b: 0 });
+		setPortLed(port, update.action === 'add' ? colors.connected : colors.idle);
 	}
 });
